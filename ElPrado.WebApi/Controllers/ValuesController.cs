51a01375f243cc9ed2379dcb16f3fbcba66b729d
hpelaez <hpelaez@gmail.com>
2024-07-16 22:33:55 -0300

    version

--- ElPrado.WebApi/Controllers/ValuesController.cs
+++ ElPrado.WebApi/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace ElPrado.WebApi.Controllers
             };
             if (ConfiguracionGeneralSesion.AllowedOrigins != null)
                 values.AddRange(ConfiguracionGeneralSesion.AllowedOrigins);
-            values.Add("Version: 11/05/2024");
+            values.Add("Version: 16/07/2024");
             return values;
         }
     }
