28dd8624845abaff78063b09d77304778d9f7210
hpelaez <hpelaez@gmail.com>
2025-07-05 20:39:00 -0300

    Detalle lugares

--- ElPrado.Dto/Dtos/DtoParcelas.cs
+++ ElPrado.Dto/Dtos/DtoParcelas.cs
@@ -14,22 +14,22 @@
     {
         public int Nivel { get; set; }
         public int Lugares { get; set; }
-        public int? CodDetInhumado1 { get; set; }
+        public string NombreInhumado1 { get; set; } = string.Empty;
         public string Estado1 { get; set; } = string.Empty;
         public string Color1 { get; set; } = "#000000";
-        public int? CodDetInhumado2 { get; set; }
+        public string NombreInhumado2 { get; set; } = string.Empty;
         public string Estado2 { get; set; } = string.Empty;
         public string Color2 { get; set; } = "#000000";
-        public int? CodDetInhumado3 { get; set; }
+        public string NombreInhumado3 { get; set; } = string.Empty;
         public string Estado3 { get; set; } = string.Empty;
         public string Color3 { get; set; } = "#000000";
-        public int? CodDetInhumado4 { get; set; }
+        public string NombreInhumado4 { get; set; } = string.Empty;
         public string Estado4 { get; set; } = string.Empty;
         public string Color4 { get; set; } = "#000000";
-        public int? CodDetInhumado5 { get; set; }
+        public string NombreInhumado5 { get; set; } = string.Empty;
         public string Estado5 { get; set; } = string.Empty;
         public string Color5 { get; set; } = "#000000";
-        public int? CodDetInhumado6 { get; set; }
+        public string NombreInhumado6 { get; set; } = string.Empty;
         public string Estado6 { get; set; } = string.Empty;
         public string Color6 { get; set; } = "#000000";
     }
