276a0fbc78dd4d2e7f7467b87ba81621ca7afcff
hpelaez <hpelaez@gmail.com>
2025-07-12 16:34:36 -0300

    Se agrega es Usuario

--- ElPrado.Services/Mappers/MenusMapper.cs
+++ ElPrado.Services/Mappers/MenusMapper.cs
@@ -6,30 +6,32 @@ namespace ElPrado.Services.Mappers
 {
     public static class MenusMapper
     {
-        public static DtoMenus MapToDto(MenusWeb menusWeb)
+        public static DtoMenus MapToDto(MenusWeb menusWeb, bool esUsuario)
         {
             return new DtoMenus()
             {
+                EsUsuario = esUsuario,
                 EsDivision = menusWeb.EsDivision,
                 Etiqueta = menusWeb.Etiqueta,
                 Icono = menusWeb.PathIcono,
                 Link = menusWeb.Proceso,
-                Items = MenusItemsMapper.MapToDto(menusWeb.Items)
+                Items = MenusItemsMapper.MapToDto(menusWeb.Items, esUsuario)
             };
         }
 
-        public static List<DtoMenus> MapToDto(List<MenusWeb> listMenusWeb)
+        public static List<DtoMenus> MapToDto(List<MenusWeb> listMenusWeb, bool esUsuario)
         {
-            return listMenusWeb.Select(item => MapToDto(item)).AsList();
+            return listMenusWeb.Select(item => MapToDto(item, esUsuario)).AsList();
         }
     }
 
     public static class MenusItemsMapper
     {
-        public static DtoMenusItems MapToDto(MenusWeb menusWeb)
+        public static DtoMenusItems MapToDto(MenusWeb menusWeb, bool esUsuario)
         {
             return new DtoMenusItems()
             {
+                EsUsuario = esUsuario,
                 EsDivision = menusWeb.EsDivision,
                 Etiqueta = menusWeb.Etiqueta,
                 Icono = menusWeb.PathIcono,
@@ -37,19 +39,20 @@ namespace ElPrado.Services.Mappers
             };
         }
 
-        public static List<DtoMenusItems>? MapToDto(List<MenusWeb>? listMenusWeb)
+        public static List<DtoMenusItems>? MapToDto(List<MenusWeb>? listMenusWeb, bool esUsuario)
         {
             if (listMenusWeb == null) return null;
-            return listMenusWeb.Select(item => MapToDto(item)).AsList();
+            return listMenusWeb.Select(item => MapToDto(item, esUsuario)).AsList();
         }
     }
 
     public static class PanelMapper
     {
-        public static DtoPanel MapToDto(MenusWeb menusWeb)
+        public static DtoPanel MapToDto(MenusWeb menusWeb, bool esUsuario)
         {
             return new DtoPanel()
             {
+                EsUsuario = esUsuario,
                 Descripcion = menusWeb.Descripcion,
                 Etiqueta = menusWeb.Etiqueta,
                 Icono = menusWeb.PathIcono,
@@ -57,9 +60,9 @@ namespace ElPrado.Services.Mappers
             };
         }
 
-        public static List<DtoPanel> MapToDto(List<MenusWeb> listMenusWeb)
+        public static List<DtoPanel> MapToDto(List<MenusWeb> listMenusWeb, bool esUsuario)
         {
-            return listMenusWeb.Select(item => MapToDto(item)).AsList();
+            return listMenusWeb.Select(item => MapToDto(item, esUsuario)).AsList();
         }
     }
 }
