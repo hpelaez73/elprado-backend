b51636fa9f105ab7da51f79a7d1d51c06bfe80e5
hpelaez <hpelaez@gmail.com>
2025-10-04 21:19:57 -0300

    Ajuste en registrar cliente

--- ElPrado.Data/Repositories/ClientesRepository.cs
+++ ElPrado.Data/Repositories/ClientesRepository.cs
@@ -43,15 +43,7 @@ namespace ElPrado.Data.Repositories
 
         public Clientes? Buscar(int legajo, long dniCuit)
         {
-            string sql = @"SELECT C.*
-                FROM CLIENTES C
-                WHERE (C.NRO_DOCUMENTO = @dniCuit OR C.CUIT = @dniCuit)
-                AND C.FECHA_BAJA IS NULL AND C.FECHA_FALLECIMIENTO IS NULL
-                AND EXISTS (SELECT 1 FROM PROPUESTA P
-                INNER JOIN PROPUESTAS_TITULARES PT ON PT.COD_PROPUESTA = P.COD_PROPUESTA
-                INNER JOIN ESTADOS_DEUDAS ED ON ED.COD_ESTADO_DEUDA = P.COD_ESTADO_DEUDA
-                WHERE PT.FECHA_BAJA IS NULL AND P.FECHA_BAJA IS NULL AND ED.ACTIVA = 1
-                AND P.LEGAJO = @legajo)";
+            string sql = @"SELECT * FROM GET_DATOS_TITULAR_DNI_CUIT(@legajo, @dniCuit)";
             return _connection.QuerySingleOrDefault<Clientes?>(sql, new { dniCuit, legajo }, _transaction);
         }
 
